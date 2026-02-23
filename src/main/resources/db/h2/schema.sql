ec92451f21d51585cf75040c7659709dd486bcff
khine ei phyu <khine ei phyu@Khing.lan>
2026-02-23 22:22:53 +0900

    コミット3　companyの桁数は50桁に変更、インデントを揃える

--- src/main/resources/db/h2/schema.sql
+++ src/main/resources/db/h2/schema.sql
@@ -40,7 +40,7 @@ CREATE TABLE owners (
   address    VARCHAR(255),
   city       VARCHAR(80),
   telephone  VARCHAR(20),
-  company	 VARCHAR(255)
+  company	 VARCHAR(50)
 
 );
 CREATE INDEX owners_last_name ON owners (last_name);
